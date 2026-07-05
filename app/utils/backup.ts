c29630d50db7ae0c1223fce8e16129b744913862
yavuz dogan <y.selimdogan@gmail.com>
2026-07-06 01:22:45 +0300

    backup works

--- app/utils/backup.ts
+++ app/utils/backup.ts
@@ -1,4 +1,4 @@
-import * as FileSystem from 'expo-file-system';
+import * as FileSystem from 'expo-file-system/legacy';
 import * as Sharing from 'expo-sharing';
 import * as DocumentPicker from 'expo-document-picker';
 import { Alert } from 'react-native';
